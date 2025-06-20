9ce6b0e4d1ea1954b94068d9e257ff0b211eaebb
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2025-06-20 10:21:52 -0700

    Add cli entrypoint and main

---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dexter2389/go-tailwind-sorter/internal/config"
+	"github.com/dexter2389/go-tailwind-sorter/internal/service"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var check bool
+var verbose bool
+var configFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "tailwind-sorter [PATH...]",
+	Short: "A Go-based sorter for Tailwind CSS classes.",
+	Long:  "A fast, standalone binary to sort Tailwind CSS classes in your project files.",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		config, err := config.New(configFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, color.RedString("Error loading configuration: %v", err))
+			os.Exit(1)
+		}
+
+		sorterService, err := service.SorterServiceNew(config, check, verbose)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, color.RedString("Error initializing sorter: %v", err))
+			os.Exit(1)
+		}
+
+		if err := sorterService.Run(args); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&configFile, "config", "", "Path to a custom TOML config file.")
+
+	rootCmd.Flags().BoolVar(&check, "check", false, "Check if files are sorted without making changes.")
+	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output.")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dexter2389/go-tailwind-sorter/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
